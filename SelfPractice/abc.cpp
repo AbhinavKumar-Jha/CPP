56366eaf721318c9de0aa1332d2c2aeb5a146ec6
Abhinav <abhinavkumarjha02@gmail.com>
2024-12-14 19:43:15 +0530

    .

---
+++ SelfPractice/abc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the value of a :";
+    cin>>a;
+    int b;
+    cout<<"Enter the value of b :";
+    cin>>b;
+    int sum = a+b;
+    cout<<"The sum of a and b is :"<<sum;
+    return 0;
+}
