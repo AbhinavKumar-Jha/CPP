5b096f22d16457b020f7e6288cdfd0c0212b8d4a
Abhinav <abhinavkumarjha02@gmail.com>
2024-12-18 19:36:34 +0530

    Wave Matrix

---
+++ 2dArray2/wavePrint1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter the number of rows :";
+    cin>>m;
+    int n;
+    cout<<"Enter the number of columns :";
+    cin>>n;
+    int arr[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+    // Wave print
+    for(int i=0;i<m;i++){
+        if(i%2==0){
+            for(int j=0;j<n;j++){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        else{ // i=1,3,5(ODD NUMBERS)
+            for(int j=n-1;j>=0;j--){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        // cout<<endl;
+    }
+    return 0;
+}
