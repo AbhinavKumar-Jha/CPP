5f87d72c7b3d104b858588607cf29273de973031
Abhinav <abhinavkumarjha02@gmail.com>
2024-12-20 20:13:11 +0530

    .

---
+++ Assignment/multiplication.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a number :";
+    cin>>a;
+    int b;
+    cout<<"Enter the number :";
+    cin>>b;
+    int n = a*b;
+    cout<<"The multiplication of a and b is :"<<n;
+    return 0;
+}
