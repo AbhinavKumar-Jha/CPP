84259dbf89097795beb1f1eaf76981831fc7eabc
Abhinav <abhinavkumarjha02@gmail.com>
2024-12-24 11:50:50 +0530

    .

---
+++ Assignment/helloWorld.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<11;i++){
+//         cout<<i<<endl;
+//     }
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World !";
+    return 0;
+}
