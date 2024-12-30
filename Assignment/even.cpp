c08b6c46ce73ca77cefc5c1f4c96ed671a879b2f
Abhinav <abhinavkumarjha02@gmail.com>
2024-12-30 11:10:31 +0530

    .

---
+++ Assignment/even.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+    if(n%2==0){
+        cout<<n<<" is Even";
+    }
+    else{
+        cout<<n<<" is Odd";
+    }
+    return 0;
+}
