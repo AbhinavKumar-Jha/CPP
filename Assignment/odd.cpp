c7580ab5db704cb088c6143c0eecff5ba4209fbc
Abhinav <abhinavkumarjha02@gmail.com>
2024-12-31 10:13:10 +0530

    .

---
+++ Assignment/odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number :";
+    cin>>n;
+    if(n%2!=0){
+        cout<<"Odd";
+    }
+    else{
+        cout<<"Even";
+    }
+    return 0;
+}
