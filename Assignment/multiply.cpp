e19c8edf4e53974cb55c096bb23245d957307187
Abhinav <abhinavkumarjha02@gmail.com>
2025-01-05 10:29:25 +0530

    .

---
+++ Assignment/multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter first number :";
+    cin>>a;
+    int b;
+    cout<<"Enter second number :";
+    cin>>b;
+    int s=a*b;
+    cout<<s;
+}
