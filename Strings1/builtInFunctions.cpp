6d40d2bacd25102c075f12c168f1a618526305dc
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-19 23:08:47 +0530

    .

--- Strings1/builtInFunctions.cpp
+++ Strings1/builtInFunctions.cpp
@@ -7,13 +7,19 @@ int main(){
     // cout<<str.length(); // we can use this also to find the length of the string
     // int len = str.length(); // we can also use this, this is the most useful formula
 
-    string str = "abcd";
-    cout<<str<<endl;
-    str.push_back('e');
-    str.push_back('f');
-    str.push_back('g');
-//    str.push_back("def"); // We can't use this we can only push char not string
-    cout<<str<<endl;
+//     string str = "abcd";
+//     cout<<str<<endl;
+//     str.push_back('e');
+//     str.push_back('f');
+//     str.push_back('g');
+// //    str.push_back("def"); // We can't use this we can only push char not string
+//     cout<<str<<endl;
 
+    string s = "Abhinav";
+    cout<<s<<endl;
+    s.pop_back(); // it removes the last char 
+    s.pop_back();  
+    s.pop_back(); 
+    cout<<s<<endl;
     return 0;
 }
