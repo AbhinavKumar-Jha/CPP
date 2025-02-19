3f452bb1c49c8fa4c2c4a622be44f35218995e50
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-19 23:02:53 +0530

    .

--- Strings1/builtInFunctions.cpp
+++ Strings1/builtInFunctions.cpp
@@ -2,9 +2,15 @@
 #include<string>
 using namespace std;
 int main(){
-    string str = "Abhinav Kumar Jha";
-    cout<<str.size()<<endl; // This doesn't includes \0 (null) .
-    cout<<str.length(); // we can use this also to find the length of the string
-    int len = str.length(); // we can also use this 
+    // string str = "Abhinav Kumar Jha";
+    // cout<<str.size()<<endl; // This doesn't includes \0 (null) .
+    // cout<<str.length(); // we can use this also to find the length of the string
+    // int len = str.length(); // we can also use this, this is the most useful formula
+
+    string str = "abcd";
+    cout<<str<<endl;
+    str.push_back('e');
+    cout<<str<<endl;
+
     return 0;
 }
