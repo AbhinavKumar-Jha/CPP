6ea6884167b9ee4b1cfa1cd7577c75ba695d2e63
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-19 23:19:15 +0530

    .

--- Strings1/builtInFunctions.cpp
+++ Strings1/builtInFunctions.cpp
@@ -2,10 +2,14 @@
 #include<string>
 using namespace std;
 int main(){
+//  // This is for finding the length of the string
+
     // string str = "Abhinav Kumar Jha";
     // cout<<str.size()<<endl; // This doesn't includes \0 (null) .
     // cout<<str.length(); // we can use this also to find the length of the string
     // int len = str.length(); // we can also use this, this is the most useful formula
+
+// // This is for pushing char in string
 
 //     string str = "abcd";
 //     cout<<str<<endl;
@@ -15,6 +19,8 @@ int main(){
 // //    str.push_back("def"); // We can't use this we can only push char not string
 //     cout<<str<<endl;
 
+// // This is for removing last char from string
+
     // string s = "Abhinav";
     // cout<<s<<endl;
     // s.pop_back(); // it removes the last char 
@@ -22,12 +28,14 @@ int main(){
     // s.pop_back(); 
     // cout<<s<<endl;
 
-    string s = "Abhinav";
-    cout<<s<<endl;
-    string t = "Jha";
-    s = s + t; // + is used here to add two strings 
-    cout<<s<<endl;
-    s = "xyz" + s;
-    cout<<s<<endl;
+// // This is used to add the two strings or char 
+
+    // string s = "Abhinav";
+    // cout<<s<<endl;
+    // string t = "Jha";
+    // s = s + t; // + is used here to add two strings 
+    // cout<<s<<endl;
+    // s = "xyz" + s;
+    // cout<<s<<endl;
     return 0;
 }
