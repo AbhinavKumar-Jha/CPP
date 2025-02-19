3964285a0914cb223325636fcde399871b84d40a
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-19 23:05:53 +0530

    .

--- Strings1/builtInFunctions.cpp
+++ Strings1/builtInFunctions.cpp
@@ -10,6 +10,9 @@ int main(){
     string str = "abcd";
     cout<<str<<endl;
     str.push_back('e');
+    str.push_back('f');
+    str.push_back('g');
+//    str.push_back("def"); // We can't use this we can only push char not string
     cout<<str<<endl;
 
     return 0;
