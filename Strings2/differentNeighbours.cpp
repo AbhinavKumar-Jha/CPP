a52a3f4ce4c70ed29c40822f1b59b700535b6a6f
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-20 11:23:57 +0530

    .

---
+++ Strings2/differentNeighbours.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int count = 0;
+    int n = s.length();
+    for(int i=0;i<n;i++){
+        if(n==1){
+            break;
+        }
+        if(i==0){
+            if(s[i]!=s[i+1]) count++;
+        }
+        else if(i==n-1){
+            if(s[i]!=s[i-1]) count ++;
+        }
+        else if(s[i]!=s[i+1] && s[i]!=s[i-1])
+            count++;
+    }
+    cout<<count;
+    return 0;
+}
