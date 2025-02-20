284a114ce62f5c7ae9f24b7c580155710f3fc567
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-20 10:25:40 +0530

    .

--- Strings1/reverseSecondHalf.cpp
+++ Strings1/reverseSecondHalf.cpp
@@ -9,7 +9,10 @@ int main(){
     // input even length string
     getline(cin,s);
     int n = s.length();
-    reverse(s.begin()+n/2, s.end());
-    cout<<s<<endl;
+// // This is for reversing the second half of string and then printing the string
+    // reverse(s.begin()+n/2, s.end());
+    // cout<<s<<endl;
+// // This is for only printing the second half of the string
+    cout<<s.substr(n/2);
     return 0;
 }
