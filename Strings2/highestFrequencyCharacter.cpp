5f7da5ff3879fe5911fbe21396da067385f2b685
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-20 22:45:24 +0530

    Optimized way to check highest frequency character

--- Strings2/highestFrequencyCharacter.cpp
+++ Strings2/highestFrequencyCharacter.cpp
@@ -1,26 +1,56 @@
+// // Bekar tarika because time complexity jyada hai
+// #include<iostream>
+// #include<string>
+// #include<algorithm>
+// using namespace std;
+// int main(){
+//     string s = "abhinav";
+//     int max = 0;
+//     for(int i=0;i<s.length();i++){
+//         char ch = s[i];
+//         int count = 1;
+//         for(int j=i+1;j<s.length();j++){
+//             if(s[j]==s[i]) count++;
+//         }
+//         if(max<count) max = count;
+//     }
+
+//     for(int i=0;i<s.length();i++){
+//         char ch = s[i];
+//         int count = 1;
+//         for(int j=i+1;j<s.length();j++){
+//             if(s[j]==s[i]) count++;
+//         }
+//         if(count==max){
+//             cout<<ch<<" "<<max<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+
+// ab optimized tarika se solve karna hai
 #include<iostream>
+#include<vector>
 #include<string>
 #include<algorithm>
 using namespace std;
 int main(){
-    string s = "abhinav";
-    int max = 0;
+    string s = "leetcode";
+    vector<int> arr(26,0);
     for(int i=0;i<s.length();i++){
         char ch = s[i];
-        int count = 1;
-        for(int j=i+1;j<s.length();j++){
-            if(s[j]==s[i]) count++;
-        }
-        if(max<count) max = count;
+        int ascii = (int)ch;
+        arr[ascii-97]++;
     }
-
-    for(int i=0;i<s.length();i++){
-        char ch = s[i];
-        int count = 1;
-        for(int j=i+1;j<s.length();j++){
-            if(s[j]==s[i]) count++;
-        }
-        if(count==max){
+    int max = 0;
+    for(int i=0;i<26;i++){
+        if(arr[i]>max) max=arr[i];
+    }
+    for(int i=0;i<26;i++){
+        if(arr[i]==max){
+            int ascii = i+97;
+            char ch = (int)ascii;
             cout<<ch<<" "<<max<<endl;
         }
     }
