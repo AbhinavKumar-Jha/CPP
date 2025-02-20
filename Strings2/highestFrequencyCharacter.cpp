344984f0ff8aaa302a87bea1848ea51f11fea57c
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-20 22:26:39 +0530

    .

--- Strings2/highestFrequencyCharacter.cpp
+++ Strings2/highestFrequencyCharacter.cpp
@@ -13,6 +13,16 @@ int main(){
         }
         if(max<count) max = count;
     }
-    cout<<max;
+
+    for(int i=0;i<s.length();i++){
+        char ch = s[i];
+        int count = 1;
+        for(int j=i+1;j<s.length();j++){
+            if(s[j]==s[i]) count++;
+        }
+        if(count==max){
+            cout<<ch<<" "<<max<<endl;
+        }
+    }
     return 0;
 }
