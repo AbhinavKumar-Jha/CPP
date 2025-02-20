9311782caedfe742f6d1b9acb6fed211b8e30738
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-20 23:38:03 +0530

    .

---
+++ Strings2/stoi_stoll.cpp
@@ -0,0 +1,10 @@
+// stoi means string to int and stoll means string to long long
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str ="123456";
+    int x = stoi(str);
+    cout<<x;
+    return 0;
+}
