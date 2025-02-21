4e94b763c4b16cdc809a8b5f23e7a1dbc09c32c3
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-21 12:04:59 +0530

    Leetcode 205

--- Strings2/isomorphicStringsLeetcode.cpp
+++ Strings2/isomorphicStringsLeetcode.cpp
@@ -9,6 +9,14 @@
 //                 if(v[idx]==1000) v[idx]=s[i]-t[i];
 //                 else if(v[idx]!=s[i]-t[i]) return false;
 //             }
+//  //     Emptying the vector
+//           for(int i=0;i<150;i++)
+//              v[i]=1000;
+//          for(int i=0;i<s.length();i++){
+//              int idx=(int)t[i];
+//              if(v[idx]==1000) v[idx]=t[i]-s[i];
+//              else if(v[idx]!=t[i]-s[i]) return false;
+//     }
 //             return true;
 //         }
 //     };
