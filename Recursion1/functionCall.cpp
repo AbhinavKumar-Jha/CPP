19b6b329c0d4850a54b78784e0cf0d1686d1fd59
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-02 11:27:30 +0530

    .

---
+++ Recursion1/functionCall.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void greet(){
+    cout<<"Good morning"<<endl;
+    cout<<"How are you?"<<endl;
+}
+int product(int a, int b){
+    return a*b;
+}
+int main(){
+    int x = product(2,4);
+    cout<<x<<endl;
+    return 0;
+}
