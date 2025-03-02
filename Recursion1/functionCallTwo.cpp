7b0e4527dedf74c3312488ae24ce78a390337125
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-02 12:06:02 +0530

    .

--- Recursion1/functionCallTwo.cpp
+++ Recursion1/functionCallTwo.cpp
@@ -1,14 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// void gun(){
+//     cout<<"Hello CW"<<endl;
+// }
+// void fun(){
+//     cout<<"Hello PW"<<endl;
+//     gun();
+//     return; // Khatam
+// }
+// int main(){
+//     fun();
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
-void gun(){
-    cout<<"Hello CW"<<endl;
-}
-void fun(){
+void fun(int n){
+    if(n==0) return;
     cout<<"Hello PW"<<endl;
-    gun();
-    return; // Khatam
+    fun(n-1);
 }
 int main(){
-    fun();
+    fun(3);
     return 0;
 }
