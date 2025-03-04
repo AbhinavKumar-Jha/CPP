de2adce0e7b73ff9948b9529899a9a139d0aeef1
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-05 00:12:05 +0530

    Now factorial upto n numbers without using extra loops for better time complexity

--- Recursion1/factorial.cpp
+++ Recursion1/factorial.cpp
@@ -13,20 +13,36 @@
 // }
 
 
-// Now factorial of number upto n
+// // Now factorial of number upto n
+// #include<iostream>
+// using namespace std;
+// void factUpto(int n){
+//     for(int j=1;j<=n;j++){
+//         int f=1;
+//         for(int i=2;i<=j;i++){
+//             f *= i;
+//         }
+//         cout<<f<<endl;
+//     }
+// }
+// int main(){
+//     factUpto(5);
+//     return 0;
+
+// }
+
+
+// Now with better time complexity single loop
 #include<iostream>
 using namespace std;
 void factUpto(int n){
-    for(int j=1;j<=n;j++){
-        int f=1;
-        for(int i=2;i<=j;i++){
-            f *= i;
-        }
+    int f=1;
+    for(int i=1;i<=n;i++){
+        f *= i;
         cout<<f<<endl;
     }
 }
 int main(){
     factUpto(5);
     return 0;
-
 }
