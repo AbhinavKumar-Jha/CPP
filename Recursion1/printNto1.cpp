bab7aedcc95d4a0519fbd8c701d5002f6680368c
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-05 00:40:41 +0530

    Printing of a N to 1 using recursion

--- Recursion1/printNto1.cpp
+++ Recursion1/printNto1.cpp
@@ -1,11 +1,27 @@
+// #include<iostream>
+// using namespace std;
+// void print(int n){
+//     // we can do this by using loop
+//     while(n>0){
+//         cout<<n<<endl;
+//         n--;
+//     }
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter any number :";
+//     cin>>n;
+//     print(n);
+//     return 0;
+// }
+
+// But we want to do this question by using recursion
 #include<iostream>
 using namespace std;
 void print(int n){
-    // we can do this by using loop
-    while(n>0){
-        cout<<n<<endl;
-        n--;
-    }
+    if(n==0) return;
+    cout<<n<<endl;
+    print(n-1);
 }
 int main(){
     int n;
