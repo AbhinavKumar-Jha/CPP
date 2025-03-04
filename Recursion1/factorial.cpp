c4a87f9be696afc3bb9f5f5e519dbafd2d3ff4dc
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-05 00:07:33 +0530

    factorial upto n numbers

--- Recursion1/factorial.cpp
+++ Recursion1/factorial.cpp
@@ -1,13 +1,32 @@
+// #include<iostream>
+// using namespace std;
+// int fact(int n){
+//     int f=1;
+//     for(int i=2;i<=n;i++){
+//         f *= i;
+//     }
+//     return f;
+// }
+// int main(){
+//     cout<<fact(5);
+//     return 0;
+// }
+
+
+// Now factorial of number upto n
 #include<iostream>
 using namespace std;
-int fact(int n){
-    int f=1;
-    for(int i=2;i<=n;i++){
-        f *= i;
+void factUpto(int n){
+    for(int j=1;j<=n;j++){
+        int f=1;
+        for(int i=2;i<=j;i++){
+            f *= i;
+        }
+        cout<<f<<endl;
     }
-    return f;
 }
 int main(){
-    cout<<fact(5);
+    factUpto(5);
     return 0;
+
 }
