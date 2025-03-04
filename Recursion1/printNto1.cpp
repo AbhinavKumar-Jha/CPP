c9524178d311cb6e0421e50d871f2cabfd71e3d8
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-05 00:43:22 +0530

    .

--- Recursion1/printNto1.cpp
+++ Recursion1/printNto1.cpp
@@ -19,10 +19,9 @@
 #include<iostream>
 using namespace std;
 void print(int n){
-    // base case
-    if(n==0) return;
-    cout<<n<<endl;
-    print(n-1);
+    if(n==0) return; // base case
+    cout<<n<<endl; // kaam
+    print(n-1); // call
 }
 int main(){
     int n;
