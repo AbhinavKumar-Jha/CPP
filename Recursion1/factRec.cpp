614977d9c8f6241d508c784f5c5fd5c7487388f5
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-05 00:26:06 +0530

    factorial using recursion

---
+++ Recursion1/factRec.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+    // base case
+    if(n==1) return 1;
+    // recursive call
+    return n*fact(n-1);
+}
+int main(){
+    cout<<fact(6);
+    return 0;
+}
