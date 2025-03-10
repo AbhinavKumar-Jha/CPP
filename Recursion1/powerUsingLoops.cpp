9715409fd3b3f3a1c416d39417e188c39041a573
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-10 21:30:46 +0530

    .

--- Recursion1/powerUsingLoops.cpp
+++ Recursion1/powerUsingLoops.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 int power(int a, int b){
+    if(a==0 && b==0){
+        cout<<"a raised to power b is not defined";
+        return -100;
+    }
     if(a==0) return 0;
     int p=1;
     for(int i=1;i<=b;i++){
@@ -9,6 +13,6 @@ int power(int a, int b){
     return p;
 }
 int main(){
-    cout<<power(3,4);
+    cout<<power(3,0);
     return 0;
 }
