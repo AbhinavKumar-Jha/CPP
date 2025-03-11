84a9197845ea3fe372570a05a1c55143f7f9ae91
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-11 10:51:39 +0530

    .

---
+++ Recursion2/powerLog.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int power(int a, int b){
+    
+}
+int main(){
+    cout<<power(3,8);
+    return 0;
+}
---
+++ SelfPractice/example.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+    cout<<n+5;
+    return 0;
+}
