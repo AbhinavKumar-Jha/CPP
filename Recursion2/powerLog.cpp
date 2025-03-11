4cc1c26a24b859018f776a0feb17ca741ca41c82
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-11 22:20:32 +0530

    .

--- Recursion2/powerLog.cpp
+++ Recursion2/powerLog.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
-int power(int a, int b){
-    
+int power(int x, int n){
+    if(n==1) return x;
+    int ans=power(x,n/2);
+    if(n%2==0){
+        return ans*ans;
+    }
+    else{
+        return ans*ans*x;
+    }
 }
 int main(){
-    cout<<power(3,8);
+    cout<<power(3,6);
     return 0;
 }
