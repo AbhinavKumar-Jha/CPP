cdf8ddb937c9d4d4c75177e14a3523fc01763cf6
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-12 19:59:13 +0530

    Skipping a character using Recursion

---
+++ Recursion3/skipAChar.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void removeChar(string ans, string original){
+    if(original.length()==0){
+        cout<<ans;
+        return;
+    }
+    char ch=original[0];
+    if(ch=='a') removeChar(ans,original.substr(1));
+    else removeChar(ans+ch,original.substr(1));
+}
+int main(){
+    string str="raghav garg";
+    removeChar("",str);
+    return 0;
+}
