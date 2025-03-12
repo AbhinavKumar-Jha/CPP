3fb0875839625db03e0266ae934d9906eb1814a9
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-12 18:13:42 +0530

    Total possible ways of finding the maze path

---
+++ Recursion2/mazePathWays.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+// sr means starting row, er for ending row and sc for starting column and ec for ending column
+int maze(int sr, int sc, int er, int ec){
+    if(sr>er || sc>ec) return 0;
+    if(sr==er && sc==ec) return 1;
+    int rightWays = maze(sr,sc+1,er,ec);
+    int downWays = maze(sr+1,sc,er,ec);
+    int totalWays = rightWays+downWays;
+    return totalWays;
+}
+void printPath(int sr, int sc, int er, int ec, string s){
+    if(sr>er || sc>ec) return; // Destination not reached
+    if(sr==er && sc==ec){ // Destination reached
+        cout<<s<<endl;
+        return;
+    }
+    printPath(sr,sc+1,er,ec,s+'R'); // Right
+    printPath(sr+1,sc,er,ec,s+'D'); // Down
+    
+}
+int main(){
+//   // this is of 3X3 matrix, written below
+    // cout<<maze(1,1,3,3);
+    printPath(1,1,3,3,"");
+
+    return 0;
+}
