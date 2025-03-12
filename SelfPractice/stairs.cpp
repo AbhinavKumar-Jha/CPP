2d60ab8027287a8eddee3c889d7fdd32fb97292b
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-12 17:30:49 +0530

    .

---
+++ SelfPractice/stairs.cpp
@@ -0,0 +1,12 @@
+// Maximum of 2 jumps
+#include<iostream>
+using namespace std;
+int stair(int n){
+    if(n==2) return 2;
+    if(n==1) return 1;
+    return stair(n-1) + stair(n-2);
+}
+int main(){
+    cout<<stair(5);
+    return 0;
+} 
