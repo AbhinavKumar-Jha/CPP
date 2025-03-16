a77f033b515f3e8aecf66b0a98e0c167ca9098ae
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-16 17:47:03 +0530

    .

--- Strings1/ifVowel.cpp
+++ Strings1/ifVowel.cpp
@@ -5,7 +5,7 @@ int main(){
     string str = "Abhinav";
     int count = 0;
     int i = 0;
-    while(str[i]!='\0'){
+    while(str[i]!='\0'){ // this '\0' denotes that string is ended as it denotes the last of the string
         if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u'){
             count++;
         }
