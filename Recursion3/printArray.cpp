f564ffe2ba6508ec9d40a869f0aa4d71699e5756
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-17 12:13:01 +0530

    Traversing Vector

--- Recursion3/printArray.cpp
+++ Recursion3/printArray.cpp
@@ -1,13 +1,24 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-void display(int arr[], int n, int idx){
-    if(idx==n) return;
-    cout<<arr[idx]<<" ";
-    display(arr,n,idx+1);
+// void display(int arr[], int n, int idx){
+//     if(idx==n) return;
+//     cout<<arr[idx]<<" ";
+//     display(arr,n,idx+1);
+// }
+void display2(vector<int>& v, int idx){
+    if(idx==v.size()) return;
+    cout<<v[idx]<<" ";
+    display2(v,idx+1);
 }
 int main(){
    int arr[]={2,1,6,3,9,0,2,7,4};
    int n = sizeof(arr)/sizeof(arr[0]);
-   display(arr,n,0);
+   vector<int> v(n);
+   for(int i=0;i<n;i++){
+    v[i]=arr[i];
+   }
+//    display(arr,n,0);
+   display2(v,0);
     return 0;
 }
