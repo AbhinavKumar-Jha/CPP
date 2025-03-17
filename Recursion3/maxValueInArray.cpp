5e78bf554ddb95f71b2804be7c7d1c0bdff05437
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-17 12:32:36 +0530

    Storing the max value in array using recursion

--- Recursion3/maxValueInArray.cpp
+++ Recursion3/maxValueInArray.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 #include<climits>
 using namespace std;
-void printMax(int arr[],int n, int idx, int max){
-    if(idx==n){
-        cout<<max;
-        return;
-    }
-    if(max<arr[idx]) max=arr[idx];
-    printMax(arr,n,idx+1,max);
+// void printMax(int arr[],int n, int idx, int max){
+//     if(idx==n){
+//         cout<<max;
+//         return;
+//     }
+//     if(max<arr[idx]) max=arr[idx];
+//     printMax(arr,n,idx+1,max);
+// }
+int maxInArray(int arr[], int n, int idx){
+    if(idx==n) return INT_MIN;
+    return max(arr[idx],maxInArray(arr,n,idx+1));
 }
 int main(){
-    int arr[]={2,1,6,3,9,0,2,7,4};
+    int arr[]={2,1,6,3,9,0,2,57,4};
     int n = sizeof(arr)/sizeof(arr[0]);
-    printMax(arr,n,0,INT_MIN);
+//  // This is to print max value
+    // printMax(arr,n,0,INT_MIN);
+// // To store max value
+    cout<<maxInArray(arr,n,0);
     return 0;
 }
