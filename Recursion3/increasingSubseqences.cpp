1ed8017f83f854bad2124c732875dfbaf6b8a2e5
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-20 00:05:29 +0530

    Increasing Subsequences

---
+++ Recursion3/increasingSubseqences.cpp
@@ -0,0 +1,27 @@
+// Leetcode 78
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+void printSubset(int arr[], int n, int idx, vector<int> ans, int k){
+    if(idx==n){
+        if(ans.size()==k){
+            for(int i=0;i<ans.size();i++){
+                cout<<ans[i]<<" ";
+            }
+            cout<<endl;
+        }
+        return;
+    }
+    printSubset(arr,n,idx+1,ans,k);
+    ans.push_back(arr[idx]);
+    printSubset(arr,n,idx+1,ans,k);
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    vector<int> v;
+    int k=3;
+    printSubset(arr,n,0,v,k);
+    return 0;
+}
