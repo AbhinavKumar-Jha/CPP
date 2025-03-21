d924b6db4d42b7fcd185bbc1a220328cf66144fb
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-21 20:15:52 +0530

    .

---
+++ Recursion3/subArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        for(int k=i;k<n;k++){
+            for(int j=i;j<=k;j++){
+                cout<<arr[j];
+            }
+            cout<<endl;
+        }
+    }  
+    return 0;
+}
