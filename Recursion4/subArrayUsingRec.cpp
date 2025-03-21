bda187e82eed9bd7d7fff5f9400dc63c7f133f42
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-22 01:26:36 +0530

    Subarray using Recursion

--- Recursion4/subArrayUsingRec.cpp
+++ Recursion4/subArrayUsingRec.cpp
@@ -10,8 +10,16 @@ void subArray(vector<int> v, int arr[], int n, int idx){
         return;
     }
     subArray(v,arr,n,idx+1);
-    v.push_back(arr[idx]);
-    subArray(v,arr,n,idx+1);
+    if(v.size()==0 || arr[idx-1]==v[v.size()-1]){
+        v.push_back(arr[idx]);
+        subArray(v,arr,n,idx+1);
+    }
+//  // Why use this when we can use directly it in if condition by using || or sign
+    // else if(arr[idx-1]==v[v.size()-1]){
+    //     v.push_back(arr[idx]);
+    //     subArray(v,arr,n,idx+1);
+    // }
+    
 }
 int main(){
     int arr[]={1,2,3,4};
