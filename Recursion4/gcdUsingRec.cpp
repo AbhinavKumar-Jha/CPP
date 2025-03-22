4b3f5294f47d34659ae2f28e7f3bbcb81d3d2e30
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-22 13:11:07 +0530

    GCD using Recursion

---
+++ Recursion4/gcdUsingRec.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b){
+    if(a==0) return b;
+    else gcd(b%a,a);
+}
+int main(){
+    int a=27;
+    int b=45;
+    cout<<gcd(a,b);
+    return 0;
+}
