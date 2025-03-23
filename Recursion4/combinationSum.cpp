0aa783b649a3e9aa8d4181acb4afe8ef99189c4e
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-23 12:00:38 +0530

    Combination Sum

---
+++ Recursion4/combinationSum.cpp
@@ -0,0 +1,26 @@
+// Leetcode 39 (Combination Sum)
+#include<iostream>
+#include<vector>
+using namespace std;
+void combination(vector<int> v, int arr[], int n, int target, int idx){
+    if(target==0){
+        for(int i=0;i<v.size();i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    if(target<0) return;
+    for(int i=idx;i<n;i++){
+        v.push_back(arr[i]);
+        combination(v,arr,n,target-arr[i],i);
+        v.pop_back();
+    }
+}
+int main(){
+    int arr[]={2,3,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    vector<int> v;
+    combination(v,arr,n,8,0);
+    return 0;
+}
