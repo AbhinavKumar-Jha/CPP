fdeae50a4aee320212be9d7cc0c0c3d0357a2174
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-23 11:18:53 +0530

    .

--- Recursion4/generateBinary.cpp
+++ Recursion4/generateBinary.cpp
@@ -1,16 +1,32 @@
+// #include<iostream>
+// using namespace std;
+// // Global variable
+// int n = 3;
+// void generate(string s){
+//     if(s.length()==n){
+//         cout<<s<<endl;
+//         return;
+//     }
+//     generate(s+'0');
+//     generate(s+'1');
+// }
+// int main(){
+//     generate("");
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
-// Global variable
-int n = 3;
-void generate(string s){
-    if(s.length()==n){
+void generate(string s, int n){
+    if(n==0){
         cout<<s<<endl;
         return;
     }
-    generate(s+'0');
-    generate(s+'1');
+    generate(s+'0',n-1);
+    generate(s+'1',n-1);
 }
 int main(){
-    generate("");
+    int n=3;
+    generate("",n);
     return 0;
 }
