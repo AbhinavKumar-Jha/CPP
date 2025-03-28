55db99b50eff04040a99794aade37720eb378bbe
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-28 19:09:24 +0530

    Leetcode 779

---
+++ Recursion4/KthSymbolinGrammarLeetcode.cpp
@@ -0,0 +1,16 @@
+// // Leetcode 779 (K-th Symbol in Grammar)
+// class Solution {
+//     public:
+//         int kthGrammar(int n, int k) {
+//             if(n==1) return 0;
+//             if(k%2==0){ // flip, k/2
+//                 int prevAns=kthGrammar(n-1, k/2);
+//                 if(prevAns==0) return 1;
+//                 else return 0;
+//             }
+//             else{ // Same, k/2+1
+//                 int prevAns=kthGrammar(n-1, k/2+1);
+//                 return prevAns;
+//             }
+//         }
+//     };
