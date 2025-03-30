7e81e9ef32be90f9ea4f7bbed15dc98db293624e
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-30 18:39:49 +0530

    Leetcode 704

--- BinarySearch/binarySearchLeetcode.cpp
+++ BinarySearch/binarySearchLeetcode.cpp
@@ -1,4 +1,4 @@
-// // Leetcode 704 (Binary Search)
+// Leetcode 704 (Binary Search)
 // class Solution {
 //     public:
 //         int search(vector<int>& nums, int target) {
@@ -6,7 +6,7 @@
 //             int lo=0;
 //             int hi=n-1;
 //             while(lo<=hi){
-//             int mid=(lo+hi)/2;
+//             int mid=lo+(hi-lo)/2;
 //             if(nums[mid]==target) return mid;
 //             if(nums[mid]<target) lo = mid+1;
 //             if(nums[mid]>target) hi = mid-1;
