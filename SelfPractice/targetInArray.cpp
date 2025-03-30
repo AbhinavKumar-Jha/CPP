f60983f564216a35edd677b54bdad7c0dd338c58
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-30 17:48:16 +0530

    .

--- SelfPractice/targetInArray.cpp
+++ SelfPractice/targetInArray.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[8]={1,2,4,5,9,15,18,21};
+    int arr[]={1,2,4,5,9,15,18,21};
+    int n=sizeof(arr)/sizeof(arr[0]);
     int target=18;
     bool flag = false;
-    for(int i=0;i<7;i++){
+    for(int i=0;i<n;i++){
         if(arr[i]==target){
             flag=true;
             break;
