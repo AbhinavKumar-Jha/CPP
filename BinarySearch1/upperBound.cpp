0de93fa8ee12b8cd07447706c00e418201de1064
Abhinav <abhinavkumarjha02@gmail.com>
2025-03-31 17:02:31 +0530

    Upper Bound

---
+++ BinarySearch1/upperBound.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[9]={1,2,4,5,9,15,18,21,24};
+    int n=9;
+    int x=12;
+
+    int lo=0;
+    int hi=n-1;
+    bool flag=false;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(arr[mid]==x){
+            flag=true;
+            cout<<arr[mid-1];
+            break;
+        }
+       else if(arr[mid]<x) lo=mid+1;
+        else hi=mid-1;
+    }
+    if(flag==false) cout<<arr[lo];
+    return 0;
+}
