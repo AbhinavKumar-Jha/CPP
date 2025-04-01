3b35a5e47b9a7ed5e27d0fa56790ac7ca6867a96
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-01 23:15:35 +0530

    Leetcode 69

---
+++ BinarySearch1/sqrtLeetcode.cpp
@@ -0,0 +1,17 @@
+// // Leetcode 69 (Sqrt(x))
+// class Solution {
+//     public:
+//         int mySqrt(int x) {
+//             int lo=0;
+//             int hi=x;
+//             while(lo<=hi){
+//                 int mid=lo+(hi-lo)/2;
+//                 long long m = (long long)mid;
+//                 long long y = (long long)x;
+//                 if(m*m==y) return mid;
+//                 else if(m*m<y) lo=mid+1;
+//                 else hi=mid-1;
+//             }
+//             return hi;
+//         }
+//     };
