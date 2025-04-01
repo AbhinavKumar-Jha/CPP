1fb6aca085277dfa00e886fd0047ef9b22b13ce4
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-01 18:34:20 +0530

    Smallest Missing Positive number

--- BinarySearch1/smallestMissingPositive.cpp
+++ BinarySearch1/smallestMissingPositive.cpp
@@ -1,13 +1,29 @@
+
 #include<iostream>
 using namespace std;
 int main(){
     int arr[]={0,1,2,3,4,8,9,12};
     int n=8;
-    for(int i=0;i<n;i++){
-        if(i!=arr[i]){
-            cout<<i;
-            break;
+//  // Linear Approach   TC - O(n)
+    // for(int i=0;i<n;i++){
+    //     if(i!=arr[i]){
+    //         cout<<i;
+    //         break;
+    //     }
+    // }
+
+// This is Binary Search  TC - O(logn)
+    int lo=0;
+    int hi=n-1;
+    int ans=-1;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(arr[mid]==mid) lo=mid+1;
+        else{
+            ans=mid;
+            hi=mid-1;
         }
     }
+    cout<<ans;
     return 0;
 }
