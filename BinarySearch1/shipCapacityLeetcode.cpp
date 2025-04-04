10ae284ce8aad0847a07e46b3bc48f098d6e6c78
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-04 12:30:08 +0530

    Leetcode 1011

---
+++ BinarySearch1/shipCapacityLeetcode.cpp
@@ -0,0 +1,42 @@
+// // Leetcode 1011 (Capacity To Ship Packages Within D Days)
+// class Solution {
+//     public:
+//         bool check(int mid, vector<int>& weights, int days){
+//             int n=weights.size();
+//             int m=mid;
+//             int count=1;
+//             for(int i=0;i<n;i++){
+//                 if(m>=weights[i]){
+//                     m -= weights[i];
+//                 }
+//                 else{
+//                     count++;
+//                     m=mid;
+//                     m -= weights[i];
+//                 }
+//             }
+//             if(count>days) return false;
+//             else return true;
+//         }
+//         int shipWithinDays(vector<int>& weights, int days) {
+//             int n=weights.size();
+//             int max=INT_MIN;
+//             int sum=0;
+//             for(int i=0;i<n;i++){
+//                 if(max<weights[i]) max=weights[i];
+//                 sum += weights[i];
+//             }
+//             int lo=max;
+//             int hi=sum;
+//             int minCapacity=sum;
+//             while(lo<=hi){
+//                 int mid=lo+(hi-lo)/2;
+//                 if(check(mid,weights,days)){
+//                     minCapacity=mid;
+//                     hi=mid-1;
+//                 }
+//                 else lo=mid+1;
+//             }
+//             return minCapacity;
+//         }
+//     };
