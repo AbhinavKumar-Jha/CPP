4a19b2170aa6c68e5b2ab5ccd6bf99dc677ead06
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-05 22:50:46 +0530

    .

--- bubbleSort/sorting.cpp
+++ bubbleSort/sorting.cpp
@@ -7,7 +7,7 @@ int main(){
     for(int i=0;i<5;i++){
         cin>>v[i];
     }
-    sort(v.begin(), v.end()); // For Ascending Order or Increasing Order
+    sort(v.begin(), v.end()); // For Ascending Order or Increasing Order (T C - O(nlogn))
     reverse(v.begin(), v.end());  // For Descending Order or Decreasing Order
     for(int i=0;i<5;i++){
         cout<<v[i]<<" ";
