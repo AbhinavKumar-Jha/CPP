5e746e62081ba499844bebb4f981cc2d1efaedea
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-05 22:49:56 +0530

    Sorting

---
+++ bubbleSort/sorting.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int> v(5);
+    for(int i=0;i<5;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(), v.end()); // For Ascending Order or Increasing Order
+    reverse(v.begin(), v.end());  // For Descending Order or Decreasing Order
+    for(int i=0;i<5;i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
