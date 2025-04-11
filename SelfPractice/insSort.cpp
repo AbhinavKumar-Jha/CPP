c1d10bc2763034a939ccfd692ecf25192443890a
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-11 16:41:19 +0530

    .

---
+++ SelfPractice/insSort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[]={5,3,1,4,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    // insertion sort
+    for(int i=1;i<n;i++){
+        int j=i;
+        // while(j>=1){
+        //     if(arr[j]>=arr[j-1]) break;
+        //     else swap(arr[j],arr[j-1]);
+        //     j--;
+        // }
+        while(j>=1 && arr[j]<arr[j-1]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
