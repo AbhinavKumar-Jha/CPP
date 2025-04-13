d10e99a1e93e0adb6ea950d97ccf44f27c4a7854
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-13 21:54:46 +0530

    Leetcode 169

---
+++ selection_insertion/majorityElementLeetcode.cpp
@@ -0,0 +1,9 @@
+// // Leetcode 169 (Majority Element)
+// class Solution {
+//     public:
+//         int majorityElement(vector<int>& nums) {
+//             int n=nums.size();
+//             sort(nums.begin(), nums.end());
+//             return nums[n/2];
+//         }
+//     };
