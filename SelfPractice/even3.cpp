a593ba7e90c2e0b3a86d75ee1264c6eada3af52c
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-18 20:04:08 +0530

    .

---
+++ SelfPractice/even3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+    if(n%2==0){
+        cout<<n<<" is Even Number!";
+    }
+    else{
+        cout<<"Odd";
+    }
+    return 0;
+}
