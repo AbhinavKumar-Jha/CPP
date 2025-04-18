1489aa666da03ed1b4d9863edaa1140fab06a70c
Abhinav <abhinavkumarjha02@gmail.com>
2025-04-19 00:09:12 +0530

    .

---
+++ SelfPractice/ex2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a :";
+    cin>>a;
+    int b;
+    cout<<"Enter b :";
+    cin>>b;
+    int product = a*b;
+    cout<<"The product of a and b is :"<<product;
+    return 0;
+}
