41b1bbb642dda7e625486cb3dfcb7f3911e181d9
Abhinav <abhinavkumarjha02@gmail.com>
2025-05-24 15:41:56 +0530

    Anagram Leetcode

---
+++ SelfPractice/anagramLeetcode.cpp
@@ -0,0 +1,14 @@
+// Leetcode 242 (Valid Anangram)
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s = "physicswallah";
+    string t = "wallahphysics";
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    if(s==t) cout<<true;
+    else cout<<false;
+    return 0;
+}
