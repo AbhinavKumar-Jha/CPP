b0e629c044a0371d3a7776b36d579306008726bb
Abhinav <abhinavkumarjha02@gmail.com>
2025-08-19 16:48:37 +0530

    Leetcode 1052

---
+++ slidingWindow/grumpyBookstoreOwnerLeetcode.cpp
@@ -0,0 +1,40 @@
+// // Leetcode 1052 (Grumpy Bookstore Owner)
+
+// class Solution {
+// public:
+//     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+//         int k = minutes;
+//         vector<int> &arr = customers;
+//         int n = arr.size();
+//         int prevLoss = 0;
+//         for(int i=0;i<k;i++){
+//             if(grumpy[i]==1) prevLoss += arr[i];
+//         }
+//         int maxLoss = prevLoss;
+//         int maxIdx=0;
+//         int i=1;
+//         int j=k;
+//         while(j<n){
+//             int currLoss = prevLoss;
+//             if(grumpy[j]==1) currLoss += arr[j];
+//             if(grumpy[i-1]==1) currLoss -= arr[i-1];
+//             if(maxLoss<currLoss){
+//                 maxLoss = currLoss;
+//                 maxIdx=i;
+//             }
+//             prevLoss = currLoss;
+//             i++;
+//             j++;
+//         }
+//         // filling 0s in the grumpy array window
+//         for(int i=maxIdx;i<maxIdx+k;i++){
+//             grumpy[i]=0;
+//         }
+//         // sum of satisfaction
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             if(grumpy[i]==0) sum += arr[i];
+//         }
+//         return sum;
+//     }
+// };
