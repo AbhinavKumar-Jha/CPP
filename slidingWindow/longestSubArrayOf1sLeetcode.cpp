a17a1a75a6002e449f86d80d0e649ed1bc022c88
Abhinav <abhinavkumarjha02@gmail.com>
2025-08-20 21:23:17 +0530

    Leetcode 1493

---
+++ slidingWindow/longestSubArrayOf1sLeetcode.cpp
@@ -0,0 +1,32 @@
+// // Leetcode 1493 (Longest Subarray of 1's After Deleting One Element)
+
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums) {
+//         int n = nums.size();
+//         int k=1;
+//         int flips = 0, i=0,j=0;
+//         int maxLen=INT_MIN, len=INT_MIN;
+//         while(j<n){
+//             if(nums[j]==1) j++;
+//             else{ // nums[j]==0
+//                 if(flips<k){
+//                     flips++;
+//                     j++;
+//                 }
+//                 else{ // flips == k
+//                     // claculate len
+//                     len = j-i;
+//                     maxLen= max(maxLen, len);
+//                     // i ko just uske aage wale 0 se ek idx aage le jaao
+//                     while(nums[i]==1) i++; // after this idx aage le jaao
+//                     i++;
+//                     j++;
+//                 }
+//             }
+//         }
+//         len = j-i;
+//         maxLen= max(maxLen, len);
+//         return maxLen-1;
+//     }
+// };
