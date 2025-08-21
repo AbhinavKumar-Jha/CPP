ba2675c53d815619593ade0884b00f0aeb49deb5
Abhinav <abhinavkumarjha02@gmail.com>
2025-08-21 09:38:45 +0530

    Number of set bits

---
+++ BitwiseAndModulo/numberOfSetBits.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+int count_set_bits(int n){
+    return __builtin_popcount(n);
+}
+int count_set_bits2(int n){
+    int count = 0;
+    while(n>0){
+        count++;
+        n=(n&(n-1));
+    }
+    return count;
+}
+using namespace std;
+int main(){
+    cout<<count_set_bits(13)<<endl;
+    cout<<count_set_bits2(64)<<endl;
+    return 0;
+}
