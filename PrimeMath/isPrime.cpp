1111fe66895db7e8b2f9a492dbd2c64f9d970ec2
Abhinav <abhinavkumarjha02@gmail.com>
2025-08-26 16:05:24 +0530

    Checking the number is prime or not

---
+++ PrimeMath/isPrime.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+bool isPrime(int n){
+    if(n==1) return false;
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+int main(){
+    cout<<isPrime(2);
+    return 0;
+}
