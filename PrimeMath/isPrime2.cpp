967642e308e9a30e9ab55749b3f30920efd1440d
Abhinav <abhinavkumarjha02@gmail.com>
2025-08-28 16:40:28 +0530

    isPrime 2

--- PrimeMath/isPrime2.cpp
+++ PrimeMath/isPrime2.cpp
@@ -1,12 +1,31 @@
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+// int fac=0;
+// bool isPrime(int n){
+//     if(n==1) return false;
+//     for(int i=2;i<=sqrt(n);i++){
+//         if(n%i==0){
+//             fac=i;
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// int main(){
+//     cout<<isPrime(1001)<<endl;
+//     cout<<fac;
+//     return 0;
+// }
+
+
 #include<iostream>
 #include<cmath>
 using namespace std;
-int fac=0;
 bool isPrime(int n){
     if(n==1) return false;
     for(int i=2;i<=sqrt(n);i++){
         if(n%i==0){
-            fac=i;
             return false;
         }
     }
@@ -14,6 +33,5 @@ bool isPrime(int n){
 }
 int main(){
     cout<<isPrime(1001)<<endl;
-    cout<<fac;
     return 0;
 }
