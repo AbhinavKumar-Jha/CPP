2cbed124d357f51dbe2cad669719962606036852
Abhinav <abhinavkumarjha02@gmail.com>
2025-09-07 18:08:48 +0530

    Leetcode 19 in one go

--- LinkedList1/removeNthNodeFromEndLeetcode.cpp
+++ LinkedList1/removeNthNodeFromEndLeetcode.cpp
@@ -23,3 +23,26 @@
 //         return head;
 //     }
 // };
+
+// // What interviewer wants -> To solve this in one pass
+
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         // traverse fast to (n+1) loops
+//         for(int i=1;i<=n+1;i++){
+//             if(fast==NULL) return head->next;
+//             fast=fast->next;   
+//         }
+//         // move slow and fast at same pace
+//         while(fast!=NULL){
+//             slow = slow->next;
+//             fast = fast->next;
+//         }
+//         // now the slow is exactly at left of the nth node from end
+//         slow->next=slow->next->next;
+//         return head;
+//     }
+// };
