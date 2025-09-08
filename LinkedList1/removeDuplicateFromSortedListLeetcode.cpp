f3bc9324b1b33f95e1993b8bbed6db5b874294b8
Abhinav <abhinavkumarjha02@gmail.com>
2025-09-08 17:19:47 +0530

    Leetcode 83

---
+++ LinkedList1/removeDuplicateFromSortedListLeetcode.cpp
@@ -0,0 +1,20 @@
+// // Leetcode 83 (Remove Duplicates from Sorted List)
+
+// class Solution {
+// public:
+//     ListNode* deleteDuplicates(ListNode* head) {
+//         if(head==NULL || head->next==NULL) return head;
+//         ListNode* a = head;
+//         ListNode* b = head->next;
+//         while(b!=NULL){
+//             while(b!=NULL && b->val==a->val){
+//             b=b->next;
+//         }
+//         a->next=b;
+//         // for next round
+//         a=b;
+//         if(b!=NULL) b=b->next;
+//         }
+//         return head;
+//     }
+// };
