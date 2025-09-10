46f3257f2e9099cac758d66d9953ee4040512094
Abhinav <abhinavkumarjha02@gmail.com>
2025-09-10 08:58:56 +0530

    Leetcode 23 with better complexity

--- LinkedList1/mergeKSortedListLeetcode.cpp
+++ LinkedList1/mergeKSortedListLeetcode.cpp
@@ -38,3 +38,40 @@
 // };
 
 
+// // Better space cpmplexity
+
+// class Solution {
+// public:
+//     ListNode* merge(ListNode* a, ListNode* b) {
+//         // SC = O(1)
+//         ListNode* c = new ListNode(100);
+//         ListNode* temp = c;
+//         while(a!=NULL && b!=NULL){
+//             if(a->val <= b->val){
+//                 temp->next=a;
+//                 a=a->next;
+//                 temp=temp->next;
+//             }
+//             else{ // b->val < a->val
+//                 temp->next=b;
+//                 b=b->next;
+//                 temp=temp->next;
+//             }
+//         }
+//         if(a==NULL) temp->next=b;
+//         else temp->next=a;
+//         return c->next;
+//     }
+//     ListNode* mergeKLists(vector<ListNode*>& arr) {
+//         if(arr.size()==0) return NULL;
+//         while(arr.size()>1){
+//             ListNode* a = arr[0];
+//             arr.erase(arr.begin());
+//             ListNode* b = arr[0];
+//             arr.erase(arr.begin());
+//             ListNode* c = merge(a,b);
+//             arr.push_back(c);
+//         }
+//         return arr[0];
+//     }
+// };
