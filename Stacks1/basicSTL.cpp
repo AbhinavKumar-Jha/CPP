5a195614ed25c0f40fab01a918fdaeef6733cd5a
Abhinav <abhinavkumarjha02@gmail.com>
2025-09-17 11:01:27 +0530

    Basic STL in Stack

---
+++ Stacks1/basicSTL.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int> st;
+    // cout<<st.size()<<endl;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    // cout<<st.size()<<endl;
+    // st.pop();
+    // cout<<st.size()<<endl;
+    // cout<<st.top()<<endl;
+
+    // // printing the value of stack in reverse order -> emptying the stack
+    // while(st.size()>0){
+    //     cout<<st.top()<<" ";
+    //     st.pop();
+    // }
+
+    // get the elements back in stack agter printing/popping -> will use extra stack
+    stack<int> temp;
+    while(st.size()>0){
+        cout<<st.top()<<" ";
+        // int x=st.top();
+        temp.push(st.top());
+        st.pop();
+    }
+    // putting elements back from temp to stack
+    while(temp.size()>0){
+        // int x = temp.top();
+        st.push(temp.top());
+        temp.pop();
+    }
+    cout<<endl<<st.top();
+    return 0;
+}
