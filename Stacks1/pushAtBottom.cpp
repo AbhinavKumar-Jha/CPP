cd8069d3ffbc96f265345341502402319b2d9738
Abhinav <abhinavkumarjha02@gmail.com>
2025-09-18 09:46:35 +0530

    Push at Bottom of a Stack

---
+++ Stacks1/pushAtBottom.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void print(stack<int>& st){
+    stack<int> temp;
+    while(st.size()>0){
+        temp.push(st.top());
+        st.pop();
+    }
+    // putting elements back from temp to stack
+    while(temp.size()>0){
+        cout<<temp.top()<<" ";
+        st.push(temp.top());
+        temp.pop();
+    }
+}
+void pushAtBottom(stack<int>& st, int val){
+    stack<int> temp;
+    while(st.size()>0){
+        temp.push(st.top());
+        st.pop();
+    }
+    st.push(val);
+    while(temp.size()>0){
+        st.push(temp.top());
+        temp.pop();
+    }
+}
+int main(){
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    print(st);
+    cout<<endl;
+    pushAtBottom(st, 80);
+    print(st);
+    return 0;
+}
