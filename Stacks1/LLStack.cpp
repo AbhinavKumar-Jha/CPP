48fda9a7e54d37f3e8bef1107b628f886c4c8e3b
Abhinav <abhinavkumarjha02@gmail.com>
2025-09-22 09:35:46 +0530

    Stack using Linked List

--- Stacks1/LLStack.cpp
+++ Stacks1/LLStack.cpp
@@ -1,41 +1,51 @@
 #include<iostream>
 using namespace std;
+class Node{ // User defined data type
+public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
 class Stack{ // user defined data structure
 public:
-    int arr[5];
-    int idx;
+    Node* head;
+    int size=0;
     Stack(){
-        idx=-1;
+        head=NULL;
+        size = 0;
     }
     void push(int val){
-        if(idx==sizeof(arr)/sizeof(arr[0])-1){
-            cout<<"Stack is Full"<<endl;
-            return;
-        }
-        idx++;
-        arr[idx] = val;
+        Node* temp = new Node(val);
+        temp->next = head;
+        head = temp;
+        size++;
     }
     void pop(){
-        if(idx==-1){
+        if(head==NULL){
             cout<<"Stack is Empty!"<<endl;
             return;
         }
-        idx--;
+        head=head->next;
+        size--;
     }
     int top(){
-        if(idx==-1){
+        if(head==NULL){
             cout<<"Stack is Empty!"<<endl;
             return -1;
         }
-        return arr[idx];
+        return head->val;
     }
-    int size(){
-        return idx+1;
+    void print(Node* temp){
+        if(temp==NULL) return;
+        print(temp->next);
+        cout<<temp->val<<" ";
     }
     void display(){
-        for(int i=0;i<=idx;i++){
-            cout<<arr[i]<<" ";
-        }
+        Node* temp = head;
+        print(temp);
         cout<<endl;
     }
 };
@@ -48,7 +58,7 @@ int main(){
     st.push(40);
     st.push(50);
     st.push(60);
-    cout<<st.size()<<endl;
+    cout<<st.size<<endl;
     cout<<st.top()<<endl;
     // st.pop();
     // cout<<st.size()<<endl;
