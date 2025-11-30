42ff251c048a8533dfbacc2644ce57d06fac4089
Abhinav <abhinavkumarjha02@gmail.com>
2025-11-30 10:23:55 +0530

    Leetcode 84

---
+++ Stacks2/largestRectInHistogramLeetcode.cpp
@@ -0,0 +1,38 @@
+// // Leetcode 84 (Largest Rectangle in Histogram)
+
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& arr) {
+//         int n = arr.size();
+//         int nsi[n]; 
+//         stack<int> st; 
+//         nsi[n-1]=n;
+//         st.push(n-1);
+//         for(int i=n-2;i>=0;i--){
+//             while(st.size()>0 && arr[st.top()]>=arr[i]) st.pop();
+//            if(st.size()==0) nsi[i]=n;
+//            else nsi[i]=st.top();
+//            st.push(i); 
+//         }
+
+//         int psi[n];
+//         stack<int> gt;
+//         psi[0]=-1;
+//         gt.push(0);
+//         for(int i=1;i<n;i++){
+//             while(gt.size()>0 && arr[gt.top()]>=arr[i]) gt.pop();
+//            if(gt.size()==0) psi[i]=-1;
+//            else psi[i]=gt.top();
+//            gt.push(i); 
+//         }
+
+//         int maxArea = 0;
+//         for(int i=0;i<n;i++){
+//             int height = arr[i];
+//             int breadth = nsi[i]-psi[i]-1;
+//             int area = height * breadth;
+//             maxArea = max(maxArea,area);
+//         }
+//         return maxArea;
+//     }
+// };
