eaa11fc097f7a21a788c12e766b20c9132654e8e
Abhinav <abhinavkumarjha02@gmail.com>
2025-12-01 10:20:59 +0530

    Leetcode 155 by Brute Force

---
+++ Stacks2/minStackLeetcode.cpp
@@ -0,0 +1,38 @@
+// // Leetcode 155 (Min Stack)
+// // Method -> 1 (Brute force)
+
+// class MinStack {
+// public:
+//     stack<int> st;
+//     MinStack() { // constructor
+        
+//     }
+    
+//     void push(int val) {
+//         st.push(val);
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() { // O(1)
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         stack<int> helper;
+//         int mn = INT_MAX;
+//         while(st.size()>0){
+//             mn = min(mn,st.top());
+//             helper.push(st.top());
+//             st.pop();
+//         }
+//         while(helper.size()>0){
+//             st.push(helper.top());
+//             helper.pop();
+//         }
+//         return mn;
+//     }
+// };
+
