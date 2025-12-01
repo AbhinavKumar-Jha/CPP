668ce719d673728c8b0753039a9f6adadad4f2cb
Abhinav <abhinavkumarjha02@gmail.com>
2025-12-01 10:25:05 +0530

    Leetcode 155

--- Stacks2/minStackLeetcode.cpp
+++ Stacks2/minStackLeetcode.cpp
@@ -36,3 +36,33 @@
 //     }
 // };
 
+
+// // Method -> 2 (Best Solution)
+
+// class MinStack {
+// public:
+//     stack<int> st;
+//     stack<int> helper;
+//     MinStack() { // constructor
+        
+//     }
+    
+//     void push(int val) {
+//         st.push(val);
+//         if(helper.size()==0 || val<helper.top()) helper.push(val);
+//         else helper.push(helper.top());
+//     }
+    
+//     void pop() {
+//         st.pop();
+//         helper.pop();
+//     }
+    
+//     int top() { // O(1)
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         return helper.top();
+//     }
+// };
