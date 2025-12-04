96f2877d5e4a485505b9cee871495ce91e9d7f12
Abhinav <abhinavkumarjha02@gmail.com>
2025-12-04 10:06:42 +0530

    Postfix Evaluation

---
+++ Stacks3/postfixEvaluation.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int solve(int val1, int val2, int ch){
+    if(ch=='+') return val1+val2;
+    else if(ch=='-') return val1-val2;
+    else if(ch=='*') return val1*val2;
+    else return val1/val2;
+}
+int main(){
+    string s = "79+4*8/3-"; // postfix expression
+    // we need one stack -> values
+    stack<int> val;
+    for(int i=0;i<s.length();i++){
+        // check if s[i] is a digit (0-9)
+        if(s[i]>=48 && s[i]<=57){ // digit
+            val.push(s[i]-48);
+        }
+        else{ // s[i] it is -> *, /, +, -
+            // kaam
+            int val2 = val.top();
+            val.pop();
+            int val1 = val.top();
+            val.pop();
+            int ans = solve(val1,val2,s[i]);
+            val.push(ans);   
+        }
+    }
+    cout<<val.top()<<endl;
+    cout<<(7+9)*4/8-3;
+    return 0;
+}
