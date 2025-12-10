4c76e58a2f761da633c5c43a8dbb00df34f26769
Abhinav <abhinavkumarjha02@gmail.com>
2025-12-10 20:18:43 +0530

    Reverse of Queue using Stack

---
+++ Queue1/reverseQueueUsingStack.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+void display(queue<int>& q){
+    int n = q.size();
+    for(int i=1;i<=n;i++){
+        int x = q.front();
+        cout<<x<<" ";
+        q.pop();
+        q.push(x);
+    }
+    cout<<endl;
+}
+void reverse(queue<int>& q){
+    stack<int> st;
+    // empty the queue into stack
+    while(q.size()>0){
+        int x = q.front();
+        q.pop();
+        st.push(x);
+    }
+    // empty the stack into queue
+    while(st.size()>0){
+        int x = st.top();
+        st.pop();
+        q.push(x);
+    }
+}
+int main(){
+    queue<int> q;
+    // push
+    // pop
+    // fron -> top
+    // size, empty
+    // back
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    display(q);
+    reverse(q);
+    display(q);
+    return 0;
+}
